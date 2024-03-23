cbc6955954a26219b0b5645de077a25282f2eacf
Robert Crocker <rcrocker13@gmail.com>
2024-03-23 17:44:47 -0500

    'Add project configuration files and dependencies for Next.js, ESLint, and Prettier. (75) It establishes the project structure and code quality standards.'

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <main>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="./images/favicon-32x32.png"
+        />
+      </Head>
+      <p>Improve your front-end skills by building projects</p>
+      <p>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
+
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </main>
+  );
+}
