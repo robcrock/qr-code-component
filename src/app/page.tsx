d7f1443effb8d378f247de76e02bd82224ba961f
Robert Crocker <rcrocker13@gmail.com>
2024-03-25 10:31:03 -0500

    Resolve a few errors.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="bg-femGrey-100 flex h-screen w-screen flex-col items-center justify-center">
       <div className="flex h-[497px] w-[320px] flex-col rounded-sm bg-white p-4 shadow-[0px_25px_25px_0px_#00000005]">
         <div className="rounded-xs overflow-hidden">
-          <Image src={qrCode} alt={"QR Code Image"} priority />
+          <Image src={qrCode} alt={"QR Code Image"} priority fetchPriority="high" />
         </div>
         <div className="flex flex-col gap-4 p-4 text-center">
           <h1 className="text-femNavy text-lg font-bold ">
@@ -21,12 +21,11 @@ export default function Home() {
       <div className="attribution">
         Challenge by{" "}
         <a
-          href="https://www.frontendmentor.io/profile/robcrock"
-          target="_blank"
+          href="https://www.frontendmentor.io/"
         >
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Robert Crocker</a>.
+        . Coded by <a href="https://www.frontendmentor.io/profile/robcrock">Robert Crocker</a>.
       </div>
     </main>
   );
